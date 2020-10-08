b5a5c9068ab1158fe0dc7e1b9944807be767a7a4
Adrian <adrisanse.ass@gmail.com>
2020-10-08 21:19:31 +0200

    2 & 3 done

---
+++ ProyectoSW2020-Alumnos/php/Credits.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<meta charset="UTF-8">
+<head>
+  <?php include '../html/Head.html'?>
+</head>
+<body>
+  <?php include '../php/Menus.php' ?>
+  <section class="main" id="s1">
+    <div>
+
+      <h2>DATOS DEL AUTOR/AUTORES</h2>
+      <ul>
+        <li>Nombre y apellidos: Ander Alonso García y Adrián San Segundo Moya</li>
+        <li>Especialidad cursada en el grado: Ambos cursamos la rama de Computación</li>
+        <li>Foto o avatar:</li>
+        <img align="center" src="../images/foto2.png" height="403" width="227" title="licencia de php"/>
+        <li>Email de la UPV/EHU: asansegundo002@ikasle.ehu.eus | aalonso213@ikasle.ehu.eus</li>
+      </ul>
+      
+
+
+    </div>
+  </section>
+  <?php include '../html/Footer.html' ?>
+</body>
+</html>
