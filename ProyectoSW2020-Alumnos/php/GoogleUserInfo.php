34a40e49d29fb769352a868fa9da55b4123a4124
Adrian <adrisanse.ass@gmail.com>
2020-12-13 20:17:15 +0100

    Log In Google API

--- ProyectoSW2020-Alumnos/php/GoogleUserInfo.php
+++ ProyectoSW2020-Alumnos/php/GoogleUserInfo.php
@@ -1,5 +1,7 @@
 <?php
 session_start();
+include "IncreaseGlobalCounter.php";
+increaseGlobalCounter();
 $_SESSION["email"] = $_POST['email'];
 $_SESSION["imagen"] = $_POST['imageUrl'];
 $_SESSION["nombre"] = $_POST['name'];
